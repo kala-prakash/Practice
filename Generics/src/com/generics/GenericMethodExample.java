caf2b977d50974e4e93fdc6fc728e089d7850d68
PrakashT <kala.prakash56@gmail.com>
2022-05-05 12:20:16 +0545

    unbounded wildcards

--- Generics/src/com/generics/GenericMethodExample.java
+++ Generics/src/com/generics/GenericMethodExample.java
@@ -10,25 +10,22 @@ package com.generics;
  * @author kala
  */
 public class GenericMethodExample {
-   
+
     // A Generic method example
-    static <T> void genericDisplay(T obj)
-    {
+    static <T> void genericDisplay(T obj) {
         System.out.println(obj.getClass().getName()
-                           + " = " + obj);
+                + " = " + obj);
     }
-  
+
     // Driver method
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Calling generic method with Integer argument
         genericDisplay(11);
-  
+
         // Calling generic method with String argument
         genericDisplay("GeeksForGeeks");
-  
+
         // Calling generic method with double argument
         genericDisplay(1.0);
     }
 }
-
