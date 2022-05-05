58d8f09c15031a06c667625f6297213e1f474054
PrakashT <kala.prakash56@gmail.com>
2022-05-05 13:31:07 +0545

    lowerBounded wildcard added

--- Generics/src/com/generics/UpperBoundedWildcard.java
+++ Generics/src/com/generics/UpperBoundedWildcard.java
@@ -26,5 +26,8 @@ public class UpperBoundedWildcard {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 3, 4);
         System.out.println("The sum of Integer is : " + sum(intList));
+        
+        List<Double> dobList = Arrays.asList(1.0,2.0,3.0);
+        System.out.println("The sum of Double is : " + sum(dobList));
     }
 }
